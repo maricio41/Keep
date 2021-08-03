b3454452146538db3eff6eae05ac4cfeb2592545
Maricio Harris <maricioharris@gmail.com>
2021-08-03 15:10:03 -0400

    react

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -7,7 +7,9 @@ const userRouter = require("./users.js");
 // const { User } = require("../../db/models");
 // const { restoreUser } = require("../../utils/auth.js");
 // const { requireAuth } = require("../../utils/auth.js");
-
+router.post("/test", function (req, res) {
+  res.json({ requestBody: req.body });
+});
 router.use("/session", sessionRouter);
 
 router.use("/users", userRouter);

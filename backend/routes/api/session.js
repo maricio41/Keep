f23f8c0bdd6d08d0c7f0fa3a4f7e8218180da08c
Maricio Harris <maricioharris@gmail.com>
2021-08-03 13:02:38 -0400

    session resore route tested

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
 
-const { setTokenCookie, restorUser } = require("../../utils/auth");
+const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
 const router = express.Router();
@@ -32,6 +32,18 @@ router.post(
 router.delete("/", (_req, res) => {
   res.clearCookie("token");
   return res.json({ message: "success" });
+});
+
+//Restore the user session
+router.get("/", restoreUser, (req, res) => {
+  const { user } = req;
+  if (user) {
+    return res.json({
+      user: user.toSafeObject(),
+    });
+  } else {
+    return res.json({});
+  }
 });
 
 module.exports = router;

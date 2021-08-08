3998af6429fd798b4aeaaeebebb38e06245bc9fb
Maricio Harris <maricioharris@gmail.com>
2021-08-07 21:29:39 -0400

    creted sidebar component

---
+++ frontend/src/components/SideBar/index.js
@@ -0,0 +1,14 @@
+const SideBar = () => {
+  return (
+    <div className="sidebar-container">
+      <div>
+        <button>Notes</button>
+      </div>
+      <div>
+        <button>Archive</button>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
